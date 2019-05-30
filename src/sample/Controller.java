d34041171952fe04fdef2966062a57b87a11ae4a
KennyR <nrufino@elpuig.xeill.net>
2019-05-30 02:43:11 +0200

    Puntuaciones añadidas, ahora las estrellas se quita si se selecciona un valor inferior

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -17,6 +18,7 @@ import java.io.IOException;
 public class Controller {
 
     @FXML public ImageView E1, E2, E3, E4, E5, F1, F2, F3, F4, F5;
+    @FXML public Label P1, P2;
 
     public void changeSceenHomePageToSelectInputPage(ActionEvent event) throws IOException {
         Parent viewParent = FXMLLoader.load(getClass().getResource("SelectInputPage.fxml"));
@@ -52,69 +54,104 @@ public class Controller {
 
         System.out.println("CLICKED");
 
-        E1.addEventHandler(MouseEvent.MOUSE_CLICKED, event1 -> E1.setImage(new Image("sample/img/selected-star.png")));
-
-        E2.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+        E1.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             E1.setImage(new Image("sample/img/selected-star.png"));
-            E2.setImage(new Image("sample/img/selected-star.png"));
+            E2.setImage(new Image("sample/img/unselected-star.png"));
+            E3.setImage(new Image("sample/img/unselected-star.png"));
+            E4.setImage(new Image("sample/img/unselected-star.png"));
+            E5.setImage(new Image("sample/img/unselected-star.png"));
+            P1.setText("1");
+            event.consume();
         });
 
-        E3.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+        E2.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            E1.setImage(new Image("sample/img/selected-star.png"));
+            E2.setImage(new Image("sample/img/selected-star.png"));
+            E3.setImage(new Image("sample/img/unselected-star.png"));
+            E4.setImage(new Image("sample/img/unselected-star.png"));
+            E5.setImage(new Image("sample/img/unselected-star.png"));
+            P1.setText("2");
+            event.consume();
+        });
+
+        E3.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             E1.setImage(new Image("sample/img/selected-star.png"));
             E2.setImage(new Image("sample/img/selected-star.png"));
             E3.setImage(new Image("sample/img/selected-star.png"));
+            E4.setImage(new Image("sample/img/unselected-star.png"));
+            E5.setImage(new Image("sample/img/unselected-star.png"));
+            P1.setText("3");
+            event.consume();
         });
 
-        E4.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+        E4.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             E1.setImage(new Image("sample/img/selected-star.png"));
             E2.setImage(new Image("sample/img/selected-star.png"));
             E3.setImage(new Image("sample/img/selected-star.png"));
             E4.setImage(new Image("sample/img/selected-star.png"));
+            E5.setImage(new Image("sample/img/unselected-star.png"));
+            P1.setText("4");
+            event.consume();
         });
 
-        E5.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+        E5.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             E1.setImage(new Image("sample/img/selected-star.png"));
             E2.setImage(new Image("sample/img/selected-star.png"));
             E3.setImage(new Image("sample/img/selected-star.png"));
             E4.setImage(new Image("sample/img/selected-star.png"));
             E5.setImage(new Image("sample/img/selected-star.png"));
+            P1.setText("5");
+            event.consume();
         });
 
-        F1.addEventHandler(MouseEvent.MOUSE_CLICKED, event1 -> F1.setImage(new Image("sample/img/selected-star.png")));
+        F1.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            F1.setImage(new Image("sample/img/selected-star.png"));
+            F2.setImage(new Image("sample/img/unselected-star.png"));
+            F3.setImage(new Image("sample/img/unselected-star.png"));
+            F4.setImage(new Image("sample/img/unselected-star.png"));
+            F5.setImage(new Image("sample/img/unselected-star.png"));
+            P2.setText("1");
+            event.consume();
+        });
 
-        F2.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+        F2.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             F1.setImage(new Image("sample/img/selected-star.png"));
             F2.setImage(new Image("sample/img/selected-star.png"));
+            F3.setImage(new Image("sample/img/unselected-star.png"));
+            F4.setImage(new Image("sample/img/unselected-star.png"));
+            F5.setImage(new Image("sample/img/unselected-star.png"));
+            P2.setText("2");
+            event.consume();
         });
 
-        F3.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+        F3.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             F1.setImage(new Image("sample/img/selected-star.png"));
-            E2.setImage(new Image("sample/img/selected-star.png"));
+            F2.setImage(new Image("sample/img/selected-star.png"));
             F3.setImage(new Image("sample/img/selected-star.png"));
+            F4.setImage(new Image("sample/img/unselected-star.png"));
+            F5.setImage(new Image("sample/img/unselected-star.png"));
+            P2.setText("3");
+            event.consume();
         });
 
-        F4.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+        F4.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             F1.setImage(new Image("sample/img/selected-star.png"));
             F2.setImage(new Image("sample/img/selected-star.png"));
             F3.setImage(new Image("sample/img/selected-star.png"));
             F4.setImage(new Image("sample/img/selected-star.png"));
+            F5.setImage(new Image("sample/img/unselected-star.png"));
+            P2.setText("4");
+            event.consume();
         });
 
-        F5.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+        F5.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             F1.setImage(new Image("sample/img/selected-star.png"));
             F2.setImage(new Image("sample/img/selected-star.png"));
             F3.setImage(new Image("sample/img/selected-star.png"));
             F4.setImage(new Image("sample/img/selected-star.png"));
             F5.setImage(new Image("sample/img/selected-star.png"));
+            P2.setText("5");
+            event.consume();
         });
-
-
-
-
-
-
-
     }
-
-
 }
