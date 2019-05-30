ca78958bb06d3d6725921aa7187af54798cc1a09
KennyR <nrufino@elpuig.xeill.net>
2019-05-30 02:26:55 +0200

    Rating bar added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,22 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
 
+    @FXML public ImageView E1, E2, E3, E4, E5, F1, F2, F3, F4, F5;
 
     public void changeSceenHomePageToSelectInputPage(ActionEvent event) throws IOException {
         Parent viewParent = FXMLLoader.load(getClass().getResource("SelectInputPage.fxml"));
@@ -31,4 +37,84 @@ public class Controller {
         window.setScene(viewScene);
         window.show();
     }
+
+    public void changeSceenSelectInputPageToInputMorning(ActionEvent event) throws IOException {
+        Parent viewParent = FXMLLoader.load(getClass().getResource("InputMorning.fxml"));
+        Scene viewScene = new Scene(viewParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(viewScene);
+        window.show();
+    }
+
+    public void ratingBar() {
+
+        System.out.println("CLICKED");
+
+        E1.addEventHandler(MouseEvent.MOUSE_CLICKED, event1 -> E1.setImage(new Image("sample/img/selected-star.png")));
+
+        E2.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+            E1.setImage(new Image("sample/img/selected-star.png"));
+            E2.setImage(new Image("sample/img/selected-star.png"));
+        });
+
+        E3.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+            E1.setImage(new Image("sample/img/selected-star.png"));
+            E2.setImage(new Image("sample/img/selected-star.png"));
+            E3.setImage(new Image("sample/img/selected-star.png"));
+        });
+
+        E4.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+            E1.setImage(new Image("sample/img/selected-star.png"));
+            E2.setImage(new Image("sample/img/selected-star.png"));
+            E3.setImage(new Image("sample/img/selected-star.png"));
+            E4.setImage(new Image("sample/img/selected-star.png"));
+        });
+
+        E5.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+            E1.setImage(new Image("sample/img/selected-star.png"));
+            E2.setImage(new Image("sample/img/selected-star.png"));
+            E3.setImage(new Image("sample/img/selected-star.png"));
+            E4.setImage(new Image("sample/img/selected-star.png"));
+            E5.setImage(new Image("sample/img/selected-star.png"));
+        });
+
+        F1.addEventHandler(MouseEvent.MOUSE_CLICKED, event1 -> F1.setImage(new Image("sample/img/selected-star.png")));
+
+        F2.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+            F1.setImage(new Image("sample/img/selected-star.png"));
+            F2.setImage(new Image("sample/img/selected-star.png"));
+        });
+
+        F3.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+            F1.setImage(new Image("sample/img/selected-star.png"));
+            E2.setImage(new Image("sample/img/selected-star.png"));
+            F3.setImage(new Image("sample/img/selected-star.png"));
+        });
+
+        F4.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+            F1.setImage(new Image("sample/img/selected-star.png"));
+            F2.setImage(new Image("sample/img/selected-star.png"));
+            F3.setImage(new Image("sample/img/selected-star.png"));
+            F4.setImage(new Image("sample/img/selected-star.png"));
+        });
+
+        F5.addEventHandler(MouseEvent.MOUSE_CLICKED, event12 -> {
+            F1.setImage(new Image("sample/img/selected-star.png"));
+            F2.setImage(new Image("sample/img/selected-star.png"));
+            F3.setImage(new Image("sample/img/selected-star.png"));
+            F4.setImage(new Image("sample/img/selected-star.png"));
+            F5.setImage(new Image("sample/img/selected-star.png"));
+        });
+
+
+
+
+
+
+
+    }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,18 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    @FXML public ImageView E1, E2, E3, E4, E5;
+    @FXML public ImageView F1, F2, F3, F4, F5;
 
     @Override
     public void start(Stage stage) throws Exception{
@@ -18,6 +23,8 @@ public class Main extends Application {
 //        primaryStage.setTitle("Hello World");
 //        primaryStage.setScene(new Scene(root, 700, 500));
 //        primaryStage.show();
+
+
     }
 
 
